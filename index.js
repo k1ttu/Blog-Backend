5187ac93c25a56eb1b673433b89e35ddbe382b27
Chirag Sharma <128295328+k1ttu@users.noreply.github.com>
2024-12-23 13:19:09 +0530

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 app.use(
     cors({
-        origin: '*', // Allow only your Angular app
+        origin: 'https://chirag-writes-stuff.netlify.app', // Allow only your Angular app
         methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
         allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
     })
@@ -24,7 +24,6 @@ app.get('/',(req , res)=>{
 app.get('/blogs', async ( req , res )=>{
     try{
         const data = await Blogs.find();
-        
         res.send(data);
         console.log(data);
     }
